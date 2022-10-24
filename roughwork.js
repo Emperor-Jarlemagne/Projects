5c33586d3272e3e2a87a2436fd25f3d8880c4f3d
Emperor-Jarlemagne <104075576+Emperor-Jarlemagne@users.noreply.github.com>
2022-10-24 08:18:05 -0600

    Oct 24/2022

--- roughwork.js
+++ roughwork.js
@@ -1,47 +1,68 @@
 
-const App = (props) => {
-    const [persons, setPersons] = useState(props.persons)
-    const [newName, setNewName] = useState('')
-    const [showAll, setShowAll] = useState(true)
-  
-    const addName = (event) => {
-        event.preventDefault()
-        const nameObject = {
-          content: newName,
-//          date: new Date().toISOString(),
-          id: persons.length =0 && persons.length +1, 
-        }
 
-        setPersons(persons.concat(nameObject))
-        setNewName('')
+import {useState} from 'react'
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas', number: '+1 905-773-3122'}
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject={
+      name: newName,
+      number: newNumber,
     }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
 
-    const handleName = (event) => {
-      console.log(event.target.value)
-      setNewName(event.target.value)
-    }
+  const handleNumber = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
 
-    const Name = ({ name }) => {
-        return (
-          <div>{name.content}</div>
-        )
-      }
+  const handleName = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
-   return (
+  const showPersons = newFilter ?
+    persons.filter(persons => persons.name.toLowerCase().includes(newFilter)) :
+    persons
+
+  const Name = ({name}) => {
+    return (
+      <div>{name.content}</div>
+    )
+  }
+
+
+  return (
     <div>
-      <h1>Persons</h1>
-      <ul>
-        {persons.map(name => 
-          <Name key={name.id} name={name} />
-        )}
-      </ul>
+      <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <input 
-          value={newName} 
-          onChange={handleName} />
-        <button type="submit">Save</button>
+        <div>
+          Name: <input type="text" value={newName} onChange={handleName} />
+        </div>
+        <div>
+          Number: <input type="text" value={newNumber} onChange={handleNumber} />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
       </form>
+      <h2>Numbers</h2>
+      <div>
+      {persons.map(name => 
+          <Name key={persons.name} name={name} />
+        )}
+      </div>
     </div>
   )
 }
+
+export default App
