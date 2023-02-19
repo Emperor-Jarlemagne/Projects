1465f2992a03ba9f69bf0c387e022c5b52d17dff
Emperor-Jarlemagne <104075576+Emperor-Jarlemagne@users.noreply.github.com>
2023-02-19 15:05:46 -0800

    Optimize beforeEach function

--- notes-backend2/tests/note_api.test.js
+++ notes-backend2/tests/note_api.test.js
@@ -9,13 +9,14 @@ const Note = require('../models/note')
 
 beforeEach(async () => {
     await Note.deleteMany({})
-    let noteObject = new Note(helper.initialNotes[0])
-    await noteObject.save()
-    noteObject = new Note(helper.initialNotes[1])
-    await noteObject.save()
+    for (let note of helper.initialNotes) {
+        let noteObject = new Note(note)
+        await noteObject.save()
+      }
 })
 
 test('notes are returned as json', async () => {
+    console.log('Beginning Test')
     await api
         .get('/api/notes')
         .expect(200)
